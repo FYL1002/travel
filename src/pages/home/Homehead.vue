0ec592b7547b791bd9628aa7ecccfbea7ea04047
布小心 <906219690@qq.com>
2021-09-30 00:17:40 +0800

    begin init

---
+++ src/pages/home/Homehead.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home-head">
+        head
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Homehead',
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .home-head{
+        width: 100%;
+    }
+</style>
