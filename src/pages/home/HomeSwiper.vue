e2c91b242d95c7fc27e49238fc09601b5ef50f7f
布小心 <906219690@qq.com>
2021-10-01 19:10:38 +0800

    homeSwiper finished

---
+++ src/pages/home/HomeSwiper.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="home-swiper">
+         <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide v-for="(item,index) of swiperData" :key="index">
+                <a class="swiper-link" :href="item.linkUrl">
+                    <img class="swiper-img" :src="item.imgUrl" alt="">
+                </a>
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>           
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeSwiper",
+    data(){
+        return{
+            swiperOption: {              
+                autoplay: 2000,
+                loop: true,
+                watchOverflow: true,
+                initialSlide: 1,
+                pagination: '.swiper-pagination',
+                paginationClickable: true
+            },
+            swiperData:[
+                {
+                    linkUrl: '',
+                    imgUrl: 'https://aecpm.alicdn.com/simba/img/TB1_JXrLVXXXXbZXVXXSutbFXXX.jpg'
+                },
+                {
+                    linkUrl: '',
+                    imgUrl: 'https://img.alicdn.com/imgextra/i2/O1CN01betoyt29ZbiEDexSS_!!6000000008082-0-tps-1130-500.jpg_q100.jpg_.webp'
+                }
+            ]
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.home-swiper ::v-deep .swiper-pagination-bullet-active {
+        background: #fff !important;
+}
+.home-swiper {
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 44%;
+    background: #eee;
+
+    .swiper-container {
+        width: 100%;
+
+        .swiper-link{
+            display: block;
+            width: 100%;
+            height: 100%;
+
+             .swiper-img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+       
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
     <home-head></home-head>
+    <home-swiper></home-swiper>
+    <div style="overflow:hidden">1234士大夫</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomeHead from '@/pages/home/HomeHead'
+import HomeSwiper from '@/pages/home/HomeSwiper'
 
 export default {
   name: 'Home',
   components: {
-    HomeHead
+    HomeHead,
+    HomeSwiper
   }
 }
 </script>
+<style lang="scss" scoped>
+.home{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+}
+</style>
