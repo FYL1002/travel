1fa2f248f43307c3e08f6208cf1dc8c0088c21ff
布小心 <906219690@qq.com>
2021-10-08 09:24:26 +0800

    put city in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,27 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+let defalutCity = "北京";
+try {
+  if(localStorage.city){
+    defalutCity = localStorage.city;
+  }
+} catch (error) {
+  
+}
 
 export default new Vuex.Store({
   state: {
-    city: "北京"
+    city: defalutCity
   },
   mutations: {
     changeCity(state,city){
       state.city = city;
+      try {
+        localStorage.city = city
+      } catch (error) {
+        
+      }
     }
   },
   actions: {
